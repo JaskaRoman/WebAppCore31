27b3feabbaaf344191c1f6f9f45d28aa0d804e32
Jaška Roman <roman.jaska@riganti.cz>
2020-06-02 18:37:50 +0200

    initial commit

---
+++ WebAppCore31/DAL/StudentDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAppCore31.DAL.Entities;
+namespace WebAppCore31.DAL
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
